115dc7edb7611d08be97c084f5b83434fa57a6a5
Samuel Almanzar <samalmanzar@Samuels-MacBook-Air.local>
2018-12-21 12:26:27 -0500

    landing page done

--- src/components/pages/header.js
+++ src/components/pages/header.js
@@ -2,35 +2,189 @@ import React from "react";
 
 function header() {
   return (
-    <div class="main">
-    <div class="light">
-  <div class="top half">
-    <img src="" />
+    <div class="demo-cont">
+ 
+  <div class="fnc-slider frontPageSlider">
+    <div class="fnc-slider__slides">
+      
+      <div class="fnc-slide m--blend-green m--active-slide">
+        <div class="fnc-slide__inner">
+          <div class="fnc-slide__mask">
+            <div class="fnc-slide__mask-inner"></div>
+          </div>
+          <div class="fnc-slide__content">
+            <h2 class="fnc-slide__heading">
+              <div class="fnc-slide__heading-line">
+                <span>Black</span>
+              </div>
+              <div class="fnc-slide__heading-line">
+                <span>Widow</span>
+              </div>
+            </h2>
+            <button type="button" class="fnc-slide__action-btn">
+              Credits
+              <span data-text="Credits">Credits</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      
+      <div class="fnc-slide m--blend-dark">
+        <div class="fnc-slide__inner">
+          <div class="fnc-slide__mask">
+            <div class="fnc-slide__mask-inner"></div>
+          </div>
+          <div class="fnc-slide__content">
+            <h2 class="fnc-slide__heading">
+              <div class="fnc-slide__heading-line">
+                <span>Captain</span>
+              </div>
+              <div class="fnc-slide__heading-line">
+                <span>America</span>
+              </div>
+            </h2>
+            <button type="button" class="fnc-slide__action-btn">
+              Credits
+              <span data-text="Credits">Credits</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      
+      <div class="fnc-slide m--blend-red">
+        <div class="fnc-slide__inner">
+          <div class="fnc-slide__mask">
+            <div class="fnc-slide__mask-inner"></div>
+          </div>
+          <div class="fnc-slide__content">
+            <h2 class="fnc-slide__heading">
+              <div class="fnc-slide__heading-line">
+                <span>Dr</span>
+              </div>
+              <div class="fnc-slide__heading-line">
+                <span>Strange</span>
+              </div>
+              <button type="button" class="fnc-slide__action-btn">
+              Credits
+              <span data-text="Credits">Credits</span>
+            </button>
+            </h2>
+          </div>
+        </div>
+      </div>
+      
+      <div class="fnc-slide m--blend-blue">
+        <div class="fnc-slide__inner">
+          <div class="fnc-slide__mask">
+            <div class="fnc-slide__mask-inner"></div>
+          </div>
+          <div class="fnc-slide__content">
+            <h2 class="fnc-slide__heading">
+              <div class="fnc-slide__heading-line">
+                <span></span>
+              </div>
+              <div class="fnc-slide__heading-line">
+                <span>Thor</span>
+              </div>
+              <button type="button" class="fnc-slide__action-btn">
+              Credits
+              <span data-text="Credits">Credits</span>
+            </button>
+            </h2>
+          </div>
+        </div>
+      </div>
+
+      <div class="fnc-slide m--blend-spider">
+        <div class="fnc-slide__inner">
+          <div class="fnc-slide__mask">
+            <div class="fnc-slide__mask-inner"></div>
+          </div>
+          <div class="fnc-slide__content">
+            <h2 class="fnc-slide__heading">
+              <div class="fnc-slide__heading-line">
+                <span></span>
+              </div>
+              <div class="fnc-slide__heading-line">
+                <span>Spiderman</span>
+              </div>
+              <button type="button" class="fnc-slide__action-btn">
+              Credits
+              <span data-text="Credits">Credits</span>
+            </button>
+            </h2>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+    <nav class="fnc-nav">
+      <div class="fnc-nav__bgs">
+        <div class="fnc-nav__bg m--navbg-widow m--active-nav-bg"></div>
+        <div class="fnc-nav__bg m--navbg-captain"></div>
+        <div class="fnc-nav__bg m--navbg-strange"></div>
+        <div class="fnc-nav__bg m--navbg-thor"></div>
+        <div class="fnc-nav__bg m--navbg-spider"></div>
+      </div>
+      <div class="fnc-nav__controls">
+        <button class="fnc-nav__control">
+          Black Widow
+          <span class="fnc-nav__control-progress"></span>
+        </button>
+        <button class="fnc-nav__control">
+          Captain America
+          <span class="fnc-nav__control-progress"></span>
+        </button>
+        <button class="fnc-nav__control">
+          Dr.Strange
+          <span class="fnc-nav__control-progress"></span>
+        </button>
+        <button class="fnc-nav__control">
+          Thor
+          <span class="fnc-nav__control-progress"></span>
+        </button>
+        <button class="fnc-nav__control">
+          Spider Man
+          <span class="fnc-nav__control-progress"></span>
+        </button>
+      </div>
+    </nav>
   </div>
-  <div class="bottom half">
-    <img src="" />
-  </div>
-  <button class="super-button">Enter</button>
-  <div class="overlay"></div>
-  <div class="button-line left">
-    <div class="inner"></div>
-  </div>
-  <div class="button-line right">
-    <div class="inner"></div>
-  </div>
+  
+  <div class="demo-cont__credits">
+    <div class="demo-cont__credits-close"></div>
+    <h2 class="demo-cont__credits-heading">Login</h2>
+   
+   
+   
+   
+   
+    {/* <img src="" alt="" class="demo-cont__credits-img" />
+    <h3 class="demo-cont__credits-name">Nikolay Talanov</h3>
+
+
+    <h2 class="demo-cont__credits-heading">Based on</h2>
+    <a href="https://dribbble.com/shots/2375246-Fashion-Butique-slider-animation" target="_blank" class="demo-cont__credits-link">Concept by Kreativa Studio</a>
+    <h4 class="demo-cont__credits-blend">Global Blend Mode</h4>
+    <div class="colorful-switch">
+      <input type="checkbox" class="colorful-switch__checkbox js-activate-global-blending" id="colorful-switch-cb" />
+      <label class="colorful-switch__label" for="colorful-switch-cb">
+        <span class="colorful-switch__bg"></span>
+        <span class="colorful-switch__dot"></span>
+        <span class="colorful-switch__on">
+          <span class="colorful-switch__on__inner"></span>
+        </span>
+        <span class="colorful-switch__off"></span>
+      </label>
+      
+    </div> */}
   </div>
 </div>
+
   );
 }
-{/* <script>
-$(document).ready(function() {
-  $(document).on("click", ".super-button", function() {
-    $(".main, .half, .overlay, .button-line, .super-button, .content").addClass("active");
-    setTimeout(function() {
-      $(".main *").css("z-index", "1");
-    }, 3000);
-}):
-});
-</script> */}
+
+
+
 
 export default header;
